3af3efe0ed8cc85e43f9e34e6c79dd3e6786c1bf
glyph <>
2006-12-21 03:47:06 +0000

    This is an emergency fix to get Moe's OS X development back on-line.  It should have no impact on the repository, since this file is a duplicate anyway, and kept only for archival purposes.

--- sine/test/historic/stub_sipserver1to2.py
+++
@@ -1,7 +0,0 @@
-from axiom.test.historic.stubloader import saveStub
-from sine.sipserver import SIPServer
-def createDatabase(s):
-    SIPServer(store=s)
-
-if __name__ == '__main__':
-    saveStub(createDatabase, 10664)
